d9cbf045f19b7e9199f2915c7a4ac4f7516d3d65
Simeon Emanuilov <simeon.emanuilov@gmail.com>
2024-09-14 21:02:47 +0300

    chore: project information and dependencie

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="similarix",
+    version="0.1.0",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "requests>=2.25.0",
+    ],
+)
